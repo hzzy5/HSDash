6021b07331591b775cc0dc504e00ac7f1149d5e0
lasse.effing <lasse.effing@study.hs-duesseldorf.de>
2025-10-26 16:38:28 +0100

    player beispiel

---
+++ model/player.js
@@ -0,0 +1,20 @@
+function Player(name, x, y) {
+  this.name = name;
+  this.x = x;
+  this.y = y;
+}
+
+// Methode auf dem Prototypen definieren
+Player.prototype.move = function(dx, dy) {
+  this.x += dx;
+  this.y += dy;
+};
+
+Player.prototype.sayHello = function() {
+  console.log(`Hi, ich bin ${this.name}!`);
+};
+
+// Instanz erzeugen
+const p = new Player("Alex", 10, 20);
+p.move(5, -3);
+p.sayHello();
