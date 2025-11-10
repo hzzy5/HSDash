99c0613162494a8626c4939355b3c195805831d8
lasse.effing <lasse.effing@study.hs-duesseldorf.de>
2025-11-10 10:02:36 +0100

    zwischenstand

--- controller/controller.js
+++ controller/controller.js
@@ -5,22 +5,22 @@ let player;
 
 document.addEventListener("keydown", (e) => {
   keys[e.key] = true;
-});
+});  //Wenn eine Taste gedrückt wird, wird sie im keys objekt auf true gesetzt
 
 document.addEventListener("keyup", (e) => {
   keys[e.key] = false;
-});
+});  //Wenn eine Taste losgelassen wird, wird sie im keys objekt auf false gesetzt
 
 export function initController(playerInstance) {
     player = playerInstance;
     requestAnimationFrame(update);
-}
+} //Die initController Funktion initialisiert den Controller mit der Spielerinstanz und startet die Update-Schleife
 
 function update() {
     if (keys["a"]) player.move(-5, 0);
     if (keys["d"]) player.move(5, 0);
     requestAnimationFrame(update);
-}
+} //überbrüft in der Update Schleife ob die Tasten a oder d gedrückt wurden und bewegt den Spieler entsprechend nach links oder rechts
 
 
 
@@ -31,6 +31,8 @@ Sobald das Grundsystem funktioniert:
 Füge Kollisionen hinzu (z. B. mit Boden)
 
 Baue einen Game-Status (leben, punkte)
+
+Sound Bibliothek einbinden und Soundeffekte hinzufügen (Sound controller)
 
 Füge Animationen / Sprites hinzu
 
