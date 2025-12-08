e18c897180a98cc280eba5f4c4b13e4f5419208b
maria-smv <smirnova.maria@gmx.de>
2025-12-08 01:25:01 +0100

    versucht ein Level mit maps umzusetzen

---
+++ model/levels.js
@@ -0,0 +1,46 @@
+//Hier speichert man die Daten eines Levels als eine Art "Map". Jedes Zeichen steht für ein Element, was später gerendert wird.
+
+export var levels = [
+    {
+        name: "level01",
+        //ggf. weitere Eigenschaften
+        map: [
+            "                                                                                                        ",
+            "                                                                                                        ",
+            "                                                                                                        ",
+            "                                                                                                        ",
+            "                                                                                                        ",
+            "                                                                                                        ",
+            "                                                                                                        ",
+            "                                                                                                        ",
+            "                                                                                                        ",
+            "                                        o o o                                                            ",
+            "                                                                                                        ",
+            "                         xxxx          xxxxxx                                                            ",
+            "                         xxxx                                      xxxx                                 ",
+            "                                                               xxxx                                     ",
+            "                                                                                                        ",
+            "                                                                                                        ",
+            "                      xxxx                                                                          x   ",
+            "                  xxxx                                                                               x  ",
+            "                                                                                                      x ",
+            "                                          o                                                       x    ",
+            "                                        o   o                                                      x   ",
+            "       xx                      x      o xxxx  o                                                       x ",
+            "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+            "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+            "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+            "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+        ]
+    }
+
+];
+
+/*  Daraus wird später ein Tile-Objekt erzeugt.
+    Ein Tile ist ein Block des Levels.
+    const TILE_SIZE = 32; beschreibt einen Block im Level, der 32x32 groß ist.
+    Liegt ein Tile in der fünften Spalte und dritten Zeile, platzieren wir ihn dort:
+    x = 5 * TILE_SIZE; y = 3 * TILE_SIZE;
+
+    Die Map ist das ganze Tilesheet.
+*/
